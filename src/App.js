714596a2c0016c408f43bf0d2950efb13d452ef0
PatrickkMo <patrickjpjc@gmail.com>
2023-02-20 14:29:14 +0800

    commit 2 20/2/2023

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
+import Fire from './Fire.js';
+
+import Bonfire from './bonfire.png';
+import mountain_background from './mountainbackground.png';
+import patform from './patrickplatform.png';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
       </header>
+
+      <div className = "App-header">
+      
+
+      <div class="homescreen-container">
+        <div className="Fire"><Fire /></div>
+        
+        <img src={patform} alt="Patform" className="Patform" />
+        <img src={Bonfire} alt="Bonfire" className="Bonfire" />
+      </div>
+
+
+
+
     </div>
+
+
+        
+
+    </div>
+
   );
 }
 
---
+++ src/Fire.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from 'react';
+import './Fire.css';
+
+function Fire() {
+
+  return (
+    
+
+    
+        <div class="flame">
+        <div class="smoke smk-1"></div>
+        <div class="smoke smk-2"></div>
+        <div class="smoke smk-3"></div>
+        <div class="smoke smk-4"></div>
+        <div class="smoke smk-5"></div>
+
+        <div class="flm-part flipped red"></div>
+        <div class="flm-part orng"></div>
+        <div class="flm-part flipped ylw"></div>
+        <div class="flm-part wht"></div>
+        </div>
+        
+  );
+}
+
+export default Fire;
+
